cea92cdf849662d4fb170cee537e4e59dad14f27
mtec21-015 <92910798+mtec21-015@users.noreply.github.com>
2022-04-29 13:55:28 +0200

    Add files via upload

---
+++ mimages/main.js
@@ -0,0 +1,13 @@
+var swiper = Swiper(".home", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+},
+}
+)
